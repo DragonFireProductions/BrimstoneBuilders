c255ef3f3a42bb113a00190b91095f3e32eeb355
Tim Seaorc <timeseaorc@gmail.com>
2018-08-19 12:00:59 -0400

    Added Basic inventory Scripts

---
+++ Assets/Seaorc/Inventory/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Item",menuName ="Inventory/Item")]
+public class Item : ScriptableObject
+{
+    [SerializeField] string ItemName = "New Item";
+    [SerializeField] Sprite Icon = null;
+    [SerializeField] int StackSize = 64;
+
+    public string GetName() { return ItemName; }
+
+    public int GetStackSize() { return StackSize; }
+}
