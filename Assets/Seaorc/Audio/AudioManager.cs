90379224fb8a28f3354c263506b6ef0ff9b841ba
Tim Seaorc <timeseaorc@gmail.com>
2018-08-24 10:46:40 -0400

    Basic Audio Manager created

---
+++ Assets/Seaorc/Audio/AudioManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    static AudioManager manager;
+    [SerializeField] Sound[] Clips;
+    Dictionary<string, Sound> SoundDictionary;
+    Sound CurrentSong;
+
+    void Awake()
+    {
+        if (manager != null)
+            Destroy(this);
+        else
+            manager = this;
+
+        DontDestroyOnLoad(gameObject);
+
+        SoundDictionary = new Dictionary<string, Sound>();
+        foreach (Sound sound in Clips)
+        {
+            GameObject gameObject = new GameObject("Sound: " + sound.GetName());
+
+            gameObject.transform.SetParent(this.transform);
+
+            sound.SetSource(gameObject.AddComponent<AudioSource>());
+
+            SoundDictionary.Add(sound.GetName(), sound);
+        }
+    }
+
+    public void PlaySound(string _Sound)
+    {
+        Sound SoundToPlay = SoundDictionary[_Sound];
+
+        if (SoundToPlay != null)
+        {
+            if (!SoundToPlay.GetIsMusic())
+            {
+                SoundToPlay.PlaySound();
+            }
+            else
+                Debug.Log("The sound you tried to play is of type \"Music\" try calling PlayMusic insted");
+        }
+        else
+            Debug.Log("Sound Not Found");
+    }
+
+    public void PlayMusic(string _Sound)
+    {
+        Sound SongToPlay = SoundDictionary[_Sound];
+
+        if (SongToPlay != null)
+        {
+            if(SongToPlay.GetIsMusic())
+            {
+                if (CurrentSong != null)
+                    CurrentSong.StopSound();
+
+                CurrentSong = SongToPlay;
+                SongToPlay.PlaySound();
+            }
+            else
+                Debug.Log("The sound you tried to play is of type \"Sound\" try calling PlaySound insted");
+        }
+        else
+            Debug.Log("Sound Not Found");
+    }
+
+    public static AudioManager GetInstance() { return manager; }
+}
---
+++ Assets/Seaorc/Audio/Sound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    [SerializeField] AudioClip Clip;
+    [SerializeField] string name;
+    [SerializeField] bool IsMusic;
+    [SerializeField] bool Looping;
+    [Range(0, 1)]
+    [SerializeField] float Volume;
+    [Range(.5f, 1.5f)]
+    [SerializeField] float Pitch;
+
+    AudioSource Source;
+
+    public void SetSource(AudioSource _source)
+    {
+        Source = _source;
+
+        Source.clip = Clip;
+        Source.volume = Volume;
+        Source.pitch = Pitch;
+    }
+
+    public void PlaySound()
+    {
+        Source.Play();
+    }
+
+    public void StopSound()
+    {
+        Source.Stop();
+    }
+
+    public string GetName() { return name; }
+
+    public bool GetIsMusic() { return IsMusic; }
+}
