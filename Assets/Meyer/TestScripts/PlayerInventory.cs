b7eae9836ce97311a8a84c37ad704ada363af898
Kristal Meyer <simplyykristyy@gmail.com>
2018-09-03 21:16:26 -0400

    added items to ui screen Item name is now printed on ui screen when user presses 'I' -Need to fix duplication bug

--- Assets/Meyer/TestScripts/PlayerInventory.cs
+++ Assets/Meyer/TestScripts/PlayerInventory.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PlayerInventory : MonoBehaviour
 {
@@ -11,6 +13,12 @@ public class PlayerInventory : MonoBehaviour
     public WeaponItemList item;
 
     public static List<WeaponItem> list;
+
+
+    public static GameObject UI;
+    private GameObject itemSlot;
+
+    private bool isActive = false;
     // Use this for initialization
     void Awake() {
         if (inventory == null)
@@ -22,6 +30,14 @@ public class PlayerInventory : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
         list = item.itemList;
+
+        UI = GameObject.FindGameObjectWithTag("UI");
+        Assert.IsNotNull(UI, "Inventory UI not found");
+
+        itemSlot = GameObject.Find("InventoryContainer");
+        Assert.IsNotNull(itemSlot, "itemSlot = null");
+
+        UI.SetActive(isActive);
     }
     public WeaponItem get_item(string name)
     {
@@ -35,11 +51,34 @@ public class PlayerInventory : MonoBehaviour
         Debug.Log(name + ": ASSET NOT FOUND");
         return null;
     }
+    private void displayInventoryUI()
+    {
+        Vector3 pos = itemSlot.gameObject.transform.position;
+        float posy = itemSlot.gameObject.transform.position.y;
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            GameObject _itemSlot = Instantiate(this.itemSlot);
+            _itemSlot.transform.SetParent(itemSlot.transform.parent);
+            _itemSlot.transform.localScale = itemSlot.transform.localScale;
+            pos.y = posy;
+            _itemSlot.gameObject.transform.position = pos;
+            _itemSlot.GetComponentInChildren<TextMeshProUGUI>().text = weapons[i].objectName;
+        }
+    }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            isActive = !isActive;
+            UI.SetActive(isActive);
+            if (isActive == true)
+            {
+            displayInventoryUI();
+            }
+            
+        }
+    }
 
     public void add(WeaponItem item)
     {
