53a32666bd26335213604f522766dc9c96d19797
Tim Seaorc <timeseaorc@gmail.com>
2018-09-09 15:17:36 -0400

    moved movement code from playercontroller to Player script

--- Assets/Seaorc/BaseClasses/Player.cs
+++ Assets/Seaorc/BaseClasses/Player.cs
@@ -7,19 +7,25 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float Health;
     [SerializeField] Animator Ani;
+    [SerializeField] float WalkSpeed;
+    [SerializeField] float RunSpeed;
+    [SerializeField] float JumpForce;
+    [SerializeField] float Gravity;
+    [SerializeField] Transform Cam;
 
-    //CharacterController Controller;
+    CharacterController Controller;
+    float Y;
     //NavMeshAgent Agent;
 
     private void Awake()
     {
-        //if (GetComponent<CharacterController>())
-        //    Controller = GetComponent<CharacterController>();
-        //else
-        //{
-        //    gameObject.AddComponent<CharacterController>();
-        //    Controller = GetComponent<CharacterController>();
-        //}
+        if (GetComponent<CharacterController>())
+            Controller = GetComponent<CharacterController>();
+        else
+        {
+            gameObject.AddComponent<CharacterController>();
+            Controller = GetComponent<CharacterController>();
+        }
 
         //if (GetComponent<NavMeshAgent>())
         //    Agent = GetComponent<NavMeshAgent>();
@@ -38,14 +44,40 @@ public class Player : MonoBehaviour
             Die();
     }
 
-    public void Move()
+    void Move()
     {
-        
+        float X = Input.GetAxis("Horizontal");
+        X *= WalkSpeed;
+        float Z = Input.GetAxis("Vertical");
+        Z *= WalkSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift) && RunSpeed > 1)
+        {
+            X *= RunSpeed;
+            Z *= RunSpeed;
+        }
+
+        if (Controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                Y = JumpForce;
+            }
+            else
+                Y = 0;
+        }
+        else
+        {
+            Y -= Gravity * Time.deltaTime;
+        }
+
+
+        Controller.Move(Cam.TransformDirection(new Vector3(X, Y, Z) * Time.deltaTime));
     }
 
     void Die()
     {
-        // 
+
     }
 
     public float GetHealth() { return Health; }
