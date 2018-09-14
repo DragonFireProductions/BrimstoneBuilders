d44106ecb0e261f3ed96a4f0b1c7835bb96f69b7
Kristal Meyer <simplyykristyy@gmail.com>
2018-09-14 15:29:14 -0400

    Deleted and moved around some stuff

---
+++ Assets/Meyer/TestScripts/InventoryWindowStuff/UIInventory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInventory : MonoBehaviour
+{
+
+    public static UIInventory instance;
+
+    struct inventorySlot
+    {
+        private static GameObject InventoryContainer;
+        private static List<string> itemVariables;
+        private static RawImage image;
+    }
+    
+
+	// Use this for initialization
+	void Awake () {
+	    if (instance == null)
+	    {
+	        instance = this;
+	    }
+	    else if (instance != this)
+	        Destroy(gameObject);
+	    DontDestroyOnLoad(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
