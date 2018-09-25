f4c5035e62cfcf97d39a964152b21d359f8a9432
Tim Seaorc <timeseaorc@gmail.com>
2018-09-25 15:48:06 -0400

    Perfunctory documentation

--- Assets/Seaorc/Inventory/Item.cs
+++ Assets/Seaorc/Inventory/Item.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="New Item",menuName ="Inventory/Item")]
 public class Item : ScriptableObject
 {
+    ///<remarks>Set in inspector</remarks>
     [SerializeField] string ItemName = "New Item";
     [SerializeField] Sprite Icon = null;
     [SerializeField] int StackSize = 64;
 
+    /// <summary>
+    /// Returns Item name
+    /// </summary>
+    /// <returns></returns>
     public string GetName() { return ItemName; }
 
+    /// <summary>
+    /// Returns max stack size
+    /// </summary>
+    /// <returns></returns>
     public int GetStackSize() { return StackSize; }
 
+    /// <summary>
+    /// returns Items icon
+    /// </summary>
+    /// <returns></returns>
     public Sprite GetIcon() { return Icon; }
 }
