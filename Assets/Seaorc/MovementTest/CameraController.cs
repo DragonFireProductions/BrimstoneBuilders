ff479b2e40daac3ac8ab557ebd4b3dc07568b592
Tim Seaorc <timeseaorc@gmail.com>
2018-09-27 16:51:54 -0400

    Target support added to camera controller

--- Assets/Seaorc/MovementTest/CameraController.cs
+++ Assets/Seaorc/MovementTest/CameraController.cs
@@ -5,40 +5,53 @@ using UnityEngine.Assertions;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] PlayerController Selected;
+    [SerializeField] GameObject Selected;
     [SerializeField] float OrbitSpeed;
     [SerializeField] float MaxAngle;
     [SerializeField] float MinAngel;
     [SerializeField] float MaxZ;
     [SerializeField] float MinZ;
+    Vector3 Center;
+
     float YAngle = 0;
-     float XAngle = 45;
-     float ZOffset = 30;
+    float XAngle = 45;
+    float ZOffset = 30;
 
     private void Update()
     {
-
+        //caculate Rotation
         if (Input.GetKey(KeyCode.Q))
             YAngle -= OrbitSpeed * Time.deltaTime;
-
         if (Input.GetKey(KeyCode.E))
             YAngle += OrbitSpeed * Time.deltaTime;
-
         if (Input.GetKey(KeyCode.Mouse2))
         {
             YAngle += (Input.GetAxis("Mouse X") * OrbitSpeed) * Time.deltaTime;
             XAngle -= (Input.GetAxis("Mouse Y") * OrbitSpeed) * Time.deltaTime;
         }
-
         ZOffset -= (Input.GetAxis("Mouse ScrollWheel") * OrbitSpeed) * Time.deltaTime;
-
         XAngle = Mathf.Clamp(XAngle, MinAngel, MaxAngle);
         ZOffset = Mathf.Clamp(ZOffset, MinZ, MaxZ);
 
-        transform.position = Selected.transform.position + transform.up;
-        transform.rotation = new Quaternion(0, 0, 0, 1);
-        transform.Rotate(XAngle, YAngle, 0);
-        transform.Translate(0, 0, -ZOffset);
+        if (Selected != null)
+        {
+            transform.position = Selected.transform.position + transform.up;
+            Center = transform.position;
+            transform.rotation = new Quaternion(0, 0, 0, 1);
+            transform.Rotate(XAngle, YAngle, 0);
+            transform.Translate(0, 0, -ZOffset);
+        }
+        else
+        {
+            transform.position = Center;
+            transform.rotation = new Quaternion(0, 0, 0, 1);
+            transform.Rotate(XAngle, YAngle, 0);
+            transform.Translate(0, 0, -ZOffset);
+        }
+    }
+
+    public void SetTarget(GameObject _Target)
+    {
+        Selected = _Target;
     }
 }
-
