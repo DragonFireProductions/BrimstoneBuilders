3e690b27a148035a32e63a7a179d180f7456c9e8
Tim Seaorc <timeseaorc@gmail.com>
2018-09-27 17:16:35 -0400

    Line script added

---
+++ Assets/Seaorc/Line.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    [SerializeField] Transform Base;
+    [SerializeField] List<Transform> transforms;
+    [SerializeField] float Spacing;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            FormLine();
+        }
+    }
+
+    public void FormLine()
+    {
+        int Half = (int)(transforms.Count * .5f);
+
+
+        for (int i = 0; i < Half; i++)
+        {
+            transforms[i].position = Base.position;
+            transforms[i].rotation = Base.rotation;
+            transforms[i].Translate(Spacing * (i + 1), 0, 0);
+        }
+
+        for (int i = Half; i < transforms.Count; i++)
+        {
+            transforms[i].position = Base.position;
+            transforms[i].rotation = Base.rotation;
+            transforms[i].Translate(-Spacing * ((i + 1) - Half), 0, 0);
+        }
+    }
+
+    public void SetBase(Transform _NewBase)
+    {
+        Base = _NewBase;
+    }
+
+    public void SetSpacing(float _Distance)
+    {
+        Spacing = _Distance;
+    }
+}
