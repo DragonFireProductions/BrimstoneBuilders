6f47748f334db06436d62b0b478560918ee475d1
Michael Buck II <MichaelBuck96@yahoo.com>
2018-10-04 14:33:06 -0400

    Added a few of my older scripts
    Just added a few of my older scripts to break them down for reference

---
+++ Assets/Design/Buck/Scripts/Camera/MJB_CameraController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MJB_CameraController : MonoBehaviour
+{
+    //This is refernced to check the useKeyBoardbool
+    MJB_PlayerController playerControllerRef;
+
+    //The object that the camera will follow
+    public Transform target;
+
+    public Vector3 offset;
+
+    //How fast the camera zooms at when zooming in/out
+    public float zoomSpeed = 4f;
+
+    //The max distance the camera can zoom in to
+    public float minZoom = 5f;
+    //The max distance the camera can zoom out to
+    public float maxZoom = 15f;
+
+    public float pitch = 2;
+
+    //How fast the camera spins around the character
+    public float yawSpeed = 100f;
+
+    //Sets teh starting zoom distance
+    float currentZoom = 10f;
+    //Sets the current yaw position
+    float currentYaw = 0f;
+
+    void Start()
+    {
+        playerControllerRef = FindObjectOfType<MJB_PlayerController>();
+    }
+
+    private void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        //if(!playerControllerRef.useKeyBoard)
+        //{
+        //    currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+        //}
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
+    }
+}
