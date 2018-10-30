a1b8e2b423a67e573a0f1bd03c00f2a784ae3d05
Albin Svensson <AVSvensson@fullsail.edu>
2018-10-30 14:06:22 -0400

    Organizing

---
+++ Assets/Development/Swede/Scripts/ParticleManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    [HideInInspector] private ParticleSystem[] particleNames;
+    [SerializeField] private ParticleSystem[] Systems;
+
+    public GameObject ParticleHolder;
+    public enum states { Rain = 0, Smoke, Selected, Embers }
+
+    public static string GetName(int index)
+    {
+        states StateName = (states) index;
+        return StateName.ToString();
+    }
+    public void Start()
+    {
+        ParticleHolder = GameObject.Find("Particles");
+
+        int lastIndex = states.GetValues(typeof(states)).Cast<int>().Last();
+        particleNames = new ParticleSystem[lastIndex + 1];
+        Systems = new ParticleSystem[lastIndex + 1];
+
+        for (int i = 0; i <= lastIndex; i++)
+        {
+            particleNames[i] = GameObject.Find(GetName(i)).GetComponent<ParticleSystem>();
+        }
+        for (int i = 0; i < particleNames.Length; i++)
+        {
+            for (int j = 0; j < particleNames.Length; j++)
+            {
+                if (particleNames[i].name == GetName(j))
+                {
+                    Systems[j] = particleNames[i];
+                    Systems[j].Stop();
+                    Systems[j].gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+    public ParticleSystem Play(states state, Vector3 position)
+    {
+        GameObject newsystem = Instantiate(Systems[(int) state].gameObject, position, Systems[(int) state].gameObject.transform.rotation);
+
+        newsystem.SetActive(true);
+        newsystem.GetComponent<ParticleSystem>().Play();
+        newsystem.transform.SetParent(ParticleHolder.transform);
+        return newsystem.GetComponent<ParticleSystem>();
+    }
+    public ParticleSystem Play(states state, Vector3 position, Transform parent)
+    {
+        GameObject newsystem = Instantiate(Systems[(int)state].gameObject, position, Systems[(int) state].gameObject.transform.rotation);
+
+        newsystem.SetActive(true);
+        newsystem.GetComponent<ParticleSystem>().Play();
+        newsystem.transform.SetParent(parent);
+        return newsystem.GetComponent<ParticleSystem>();
+    }
+    public void Stop(ParticleSystem system)
+    {
+        system.Stop();
+        Destroy(system.gameObject);
+    }
+}
---
+++ Assets/Development/Swede/Scripts/TestParticle.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestParticle : MonoBehaviour
+{
+    bool active_rain = false, active_smoke = false, active_selected = false, active_embers = false;
+    ParticleSystem particle_rain = null, particle_smoke = null, particle_selected = null, particle_embers = null;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            active_rain = !active_rain;
+            if (active_rain == true)
+            {
+                particle_rain = StaticManager.particleManager.Play(ParticleManager.states.Rain, StaticManager.Character.transform.position);
+            }
+            else
+            {
+                StaticManager.particleManager.Stop(particle_rain);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            active_smoke = !active_smoke;
+            if (active_smoke == true)
+            {
+                particle_smoke = StaticManager.particleManager.Play(ParticleManager.states.Smoke, StaticManager.Character.transform.position);
+            }
+            else
+            {
+                StaticManager.particleManager.Stop(particle_smoke);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            active_selected = !active_selected;
+            if (active_selected == true)
+            {
+                particle_selected = StaticManager.particleManager.Play(ParticleManager.states.Selected, StaticManager.Character.transform.position, StaticManager.Character.transform);
+            }
+            else
+            {
+                StaticManager.particleManager.Stop(particle_selected);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            active_embers = !active_embers;
+            if (active_embers == true)
+            {
+                particle_embers = StaticManager.particleManager.Play(ParticleManager.states.Embers, StaticManager.Character.transform.position);
+            }
+            else
+            {
+                StaticManager.particleManager.Stop(particle_embers);
+            }
+        }
+    }
+}
