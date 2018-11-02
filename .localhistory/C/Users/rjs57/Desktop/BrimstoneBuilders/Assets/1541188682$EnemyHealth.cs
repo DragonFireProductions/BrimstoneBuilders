8c763025644cbb6a4bc6c84e04a181086b66d4fd
Reaperv115 <rjs5785@hotmail.com>
2018-11-02 17:01:39 -0400

    almost finished with the enemy health bar

---
+++ .localhistory/C/Users/rjs57/Desktop/BrimstoneBuilders/Assets/1541188682$EnemyHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using Kristal;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public Image HPbar;
+    public TextMeshProUGUI HPtext;
+
+    private Stat enemy;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	    enemy = gameObject.GetComponent<Enemy>().stats;
+
+	    if (enemy != null)
+	    {
+	        if (HPbar != null)
+	        {
+	            float hp = enemy.Health / enemy.MaxHealth;
+
+	            HPbar.fillAmount = hp;
+	            HPbar.color = Color.Lerp(Color.red, Color.green, hp);
+	        }
+	    }
+	}
+}
