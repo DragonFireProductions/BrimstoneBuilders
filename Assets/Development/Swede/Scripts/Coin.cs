60afce0f4c04a8b4920f307db6ee492efae97e31
Albin Svensson <AVSvensson@fullsail.edu>
2018-11-19 10:15:13 -0500

    Shop Ui rework

--- Assets/Development/Swede/Scripts/Coin.cs
+++ Assets/Development/Swede/Scripts/Coin.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public int coinWorth;
+
+
+    //Checks the trigger around the coin. If the tag matches the player, the coin gets added to the player's inventory and destroyed from the game world.
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
--- Assets/Development/Swede/Scripts/Currency.cs
+++ Assets/Development/Swede/Scripts/Currency.cs
@@ -10,17 +10,22 @@ public class Currency : MonoBehaviour
 
     public Shop _shop;
 
+    //Used when conducting shop purchases.
     public void AddCoins(Shop shop)
     {
         StaticManager.Character.inventory.coinCount += shop.resaleWorth;
     }
+
+    //Used when adding an actual coin object.
     public void AddCoins(int _coins)
     {
         StaticManager.Character.inventory.coinCount += _coins;
     }
 
+    //Used when conducting shop purchases.
     public void RemoveCoins(Shop shop)
     {
+        //if the player's coin total is less than or equal to the amount of coins
         if (StaticManager.Character.inventory.coinCount <= shop.companionPrice)
         {
             StaticManager.Character.inventory.coinCount = 0;
@@ -30,6 +35,8 @@ public class Currency : MonoBehaviour
             StaticManager.Character.inventory.coinCount -= shop.companionPrice;
         }
     }
+
+    //checks to see whether or not the player has enough coins to complete the transaction.
     public bool RemoveCoins(int coins)
     {
         if (StaticManager.Character.inventory.coinCount < coins)
@@ -45,16 +52,22 @@ public class Currency : MonoBehaviour
 
     public void BuyCompanion(Shop shop)
     {
+        //If the player still has slots available to hire a companion (max of five companions)
         if (StaticManager.Character.spawner.comp.Count < 5)
         {
+            //If the player has enough to cover the cost, adds a companion to their service.
             if(RemoveCoins(shop.companionPrice)) 
                 StartCoroutine(StaticManager.Character.spawner.CompSpawn());
         }
     }
+
+
     public void SellCompanion(Shop shop)
     {
+        //If the player has companions available to get rid of...
         if(StaticManager.Character.spawner.comp.Count != 0)
         {
+            //Refunds the player the ReSale value of a companion. Despawns the companion.
             AddCoins(shop.resaleWorth);
             StaticManager.Character.spawner.Kill();
         }
