8d623f93fd3c67e2b3821efc6a9199eba0b014b3
Albin Svensson <AVSvensson@fullsail.edu>
2018-12-12 14:16:20 -0500

    Ui Changes with companion hud and shop updates.

---
+++ Assets/Companion UI Update.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Companion_UI_Update : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Development/Swede/Scripts/CompanionUIUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Assets.Meyer.TestScripts;
+
+public class CompanionUIUpdate : MonoBehaviour
+{
+    [Header("Status")]
+    public Image HPBar;
+
+    private Stat Selected;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Selected = StaticManager.Character.stats;
+
+	    if (Selected != null)
+	    {
+	        if (HPBar != null)
+	        {
+	            float HP = Selected.Health / Selected.MaxHealth;
+
+	            HPBar.fillAmount = HP;
+	            HPBar.color = Color.Lerp(Color.red, Color.green, HP);
+	        }
+	    }
+    }
+}
