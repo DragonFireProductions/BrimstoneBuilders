a6c933434381980c25d5ae378d094cac6fb89c8c
Kristal Meyer <simplyykristyy@gmail.com>
2019-01-30 14:55:45 -0500

    stuff

--- Assets/Development/Meyer/Scripts/Player/LeaderNav.cs
+++ Assets/Development/Meyer/Scripts/Player/LeaderNav.cs
@@ -92,19 +92,18 @@ public class LeaderNav : BaseNav {
         if ( Input.GetMouseButtonDown( 1 ) && !Input.GetKey(KeyCode.LeftShift) ){
             Agent.isStopped = true;
             SetState        = state.FREEZE;
-
-            if ( character.attachedWeapon is GunType && Physics.Raycast( l_ray , out hit ) ){
+             l_ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+            int _mask = 1 << 11;
+            _mask = ~_mask;
+            if ( character.attachedWeapon is GunType && Physics.Raycast( l_ray , out hit, 1000, _mask ) ){
                 if ( hit.collider.tag != "Companion" && hit.collider.tag != "Player" ){
                     character.attachedWeapon.Use( );
-
-                    if ( State != state.MOVE ){
-                        StartCoroutine( rotate( ) );
-                    }
                 }
             }
         }
 
         if ( Input.GetMouseButtonDown( 0 ) ){
+             l_ray = Camera.main.ScreenPointToRay( Input.mousePosition );
             if ( Physics.Raycast( l_ray , out hit ) ){
                 if ( hit.collider.tag == "ShopKeeper" && !StaticManager.UiInventory.ItemsInstance.windowIsOpen ) //Left Click
                 {
@@ -176,28 +175,12 @@ public class LeaderNav : BaseNav {
                 break;
         }
     }
-    public IEnumerator rotate()
+    public IEnumerator rotate(Vector3 pos)
     {
-        Plane playerPlane = new Plane(Vector3.up, transform.position);
-
-        // Generate a ray from the cursor position
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        // Determine the point where the cursor ray intersects the plane.
-        // This will be the point that the object must look towards to be looking at the mouse.
-        // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
-        //   then find the point along that ray that meets that distance.  This will be the point
-        //   to look at.
-        float hitdist = 0.0f;
-        // If the ray is parallel to the plane, Raycast will return false.
-        if (playerPlane.Raycast(ray, out hitdist))
-        {
-
-            // Get the point along the ray that hits the calculated distance.
-            Vector3 targetPoint = ray.GetPoint(hitdist);
-
+        
+        Vector3 _pos = new Vector3(pos.x, transform.position.y, pos.z);
             // Determine the target rotation.  This is the rotation if the transform looks at the target point.
-            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            Quaternion targetRotation = Quaternion.LookRotation( transform.position - _pos);
 
             while (transform.rotation != targetRotation && State != state.MOVE)
             {
@@ -205,10 +188,6 @@ public class LeaderNav : BaseNav {
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5 * Time.deltaTime);
                 yield return new WaitForEndOfFrame();
             }
-
-
-
-        }
     }
 
     public void rotate1( ) {
