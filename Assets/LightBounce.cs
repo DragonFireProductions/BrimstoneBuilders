332bc8c83168e2cfa64a63b911e57f3bdfe1ed40
Albin Svensson <AVSvensson@fullsail.edu>
2019-01-30 13:24:18 -0500

    Fire fixed and lights adjusted.

---
+++ Assets/LightBounce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBounce : MonoBehaviour
+{
+    [SerializeField] public float Intensity_Rate;
+
+    private Vector3 StartPos;
+    private float rand;
+
+    private Light ThisLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThisLight = GetComponent<Light>();
+        rand = Random.Range(0, 65000);
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(rand, Time.time * Intensity_Rate);
+
+        transform.position = new Vector3(transform.position.x, StartPos.y + Mathf.Sin(Time.time), transform.position.z);
+
+    }
+}
